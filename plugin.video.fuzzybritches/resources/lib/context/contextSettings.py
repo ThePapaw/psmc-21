8af62eca71bfbad5b13ef0154bcd37ab5142f0c0
The Papaw <thepapaw@zizzlezazzle.com>
2024-06-14 02:13:02 -0500

    removed plugin.video.fuzzybritches test

--- plugin.video.fuzzybritches/resources/lib/context/contextSettings.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import xbmc
-
-if __name__ == '__main__':
-	plugin = 'plugin://plugin.video.fuzzybritches/'
-	path = 'RunPlugin(%s?action=tools_contextFuzzyBritchesSettings&opensettings=false)' % plugin
-	xbmc.executebuiltin(path)
-	# xbmc.executebuiltin('RunPlugin(%s?action=widgetRefresh)' % plugin) #now part of "tools_contextFuzzyBritchesSettings" action
